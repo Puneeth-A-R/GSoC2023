3f833d79c0f72210ac17c073d21e878f88fa412c
Puneeth <89592469+Puneeth-A-R@users.noreply.github.com>
2023-08-13 15:26:10 +0530

    Create FunctionAnnotation.c

---
+++ FunctionAnnotation.c
@@ -0,0 +1,24 @@
+void first_function()
+{
+    return;
+}
+
+void second_function()
+{
+    return;
+}
+
+void third_function()
+{
+    return;
+}
+
+void fourth_function()
+{
+    return;
+}
+
+void fifth_function()
+{
+    return;
+}
